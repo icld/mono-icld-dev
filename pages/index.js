981dbef0d977936786a42454a326edeb267f0c68
icld <ian@icldesign.com>
2021-11-08 20:11:16 -0800

    Get or create user at sign in.  add to state as activeUser

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import Head from 'next/head';
-
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useSession, getSession } from 'next-auth/react';
-
+import { PrismaClient } from '@prisma/client';
 import ActiveUser from 'components/users/ActiveUser';
+import { useStore } from 'lib/zustand/store';
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+export default function Home({ newActiveUser, session }) {
   const { data: user, status } = useSession();
+  const { activeUser, setActiveUser } = useStore();
   const router = useRouter();
+
+  useEffect(() => {
+    setActiveUser(newActiveUser);
+    console.log(newActiveUser);
+  }, []);
 
   if (typeof window !== 'undefined') return null;
 
@@ -16,7 +24,7 @@ export default function Home() {
     return (
       <>
         <Head>
-          <title>mweet</title>
+          <title>mweeter</title>
           <link rel='icon' href='/favicon.ico' />
         </Head>
         <div className='relative flex flex-col items-center justify-center h-screen m-auto'>
@@ -32,6 +40,13 @@ export default function Home() {
 
 export async function getServerSideProps({ req }) {
   const session = await getSession({ req });
+  const account = await prisma.user.findFirst({
+    where: {
+      email: session?.user?.email,
+    },
+  });
+  let newActiveUser;
+
   if (!session) {
     return {
       redirect: {
@@ -40,9 +55,30 @@ export async function getServerSideProps({ req }) {
       },
     };
   }
+
+  if (!account) {
+    const { name, image, email } = session.user;
+    const fullName = name.split(' ');
+    const firstName = fullName[0];
+    const lastName = fullName[fullName.length - 1];
+    const handle = `@${firstName[0]}${lastName}`;
+    newActiveUser = await prisma.user.create({
+      data: {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        image: image,
+        userName: handle,
+      },
+    });
+  } else {
+    newActiveUser = account;
+  }
+  console.log(newActiveUser);
   return {
     props: {
       session,
+      newActiveUser,
     },
   };
 }
